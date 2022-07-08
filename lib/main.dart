6736c16acc7c4300581cda1963e75bb55081be40
CaioLuppo <luppocode@outlook.com>
2022-07-08 01:23:16 -0300

    Integração das telas e preparo do 'Future'

--- lib/main.dart
+++ lib/main.dart
@@ -10,66 +10,81 @@ class ByteBankApp extends StatelessWidget {
     return MaterialApp(
       title: "ByteBank - By: Caio Luppo",
       home: Scaffold(
-        body: FormularioTransferencias(),
+        body: ListaTransferencias(),
       ),
     );
   }
 }
 
 class FormularioTransferencias extends StatelessWidget {
-
-  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
   final TextEditingController _controladorCampoValor = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Criando Transferência"),),
+        appBar: AppBar(
+          title: Text("Criando Transferência"),
+        ),
         body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),  // define a distância dos elementos
-              child: TextField(
-                controller: _controladorCampoNumeroConta,
-                style: TextStyle(
-                  fontSize: 24.0,  // Padrão é na base 8 (8, 16, 24)
-                ),
-                decoration: InputDecoration(
-                  labelText: "Número da conta",
-                  hintText: "0000",
-                ),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),  // define a distância dos elementos
-              child: TextField(
-                controller: _controladorCampoValor,
-                style: TextStyle(
-                  fontSize: 24.0,  // Padrão é na base 8 (8, 16, 24)
-                ),
-                decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: "Valor",
-                  hintText: "0.00",
-                ),
-                keyboardType: TextInputType.number,
-              ),
-            ),
+            Editor(
+                controlador: _controladorCampoNumeroConta,
+                rotulo: "Número da conta",
+                dica: "0000"),
+            Editor(
+                controlador: _controladorCampoValor,
+                rotulo: "Valor",
+                dica: "000.0",
+                icone: Icons.monetization_on),
             ElevatedButton(
               child: Text("Confirmar"),
-              onPressed: () {
-                final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
-                final double? valor = double.tryParse(_controladorCampoValor.text);
-
-                if(numeroConta != null && valor != null){
-                  final transferenciaCriada = Transferencia(valor, numeroConta);
-
-                }
-              },
+              onPressed: () => _criaTransferencia(context),
             ),
           ],
-        )
+        ));
+  }
+
+  void _criaTransferencia(BuildContext context) {
+    final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor = double.tryParse(_controladorCampoValor.text);
+
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData? icone;
+
+  Editor(
+      {required this.controlador,
+      required this.rotulo,
+      required this.dica,
+      this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0), // define a distância dos elementos
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(
+          fontSize: 24.0, // Padrão é na base 8 (8, 16, 24)
+        ),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
     );
   }
 }
@@ -87,7 +102,14 @@ class ListaTransferencias extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          final Future<Transferencia?> future = Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return FormularioTransferencias();
+          }));
+          future.then((transferenciaRecebida){
+
+          });
+        },
         child: Icon(Icons.add),
       ),
       appBar: AppBar(
